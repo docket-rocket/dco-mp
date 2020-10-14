8634c76bb98d33da12576ff8a51f265b2ff7ab6a
vivek <webdev.vivekpatel@gmail.com>
2020-10-14 14:28:18 +0530

    Updates

---
+++ precache-manifest.580ceeefd6e4b55c747941c9ea425b7d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f60b483cbe1163b8b739a855b2a3d20",
+    "url": "/dco-mp/index.html"
+  },
+  {
+    "revision": "00be22364b13e7f2d9de",
+    "url": "/dco-mp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6256ecde17ff55c415a0",
+    "url": "/dco-mp/static/css/main.58941df9.chunk.css"
+  },
+  {
+    "revision": "00be22364b13e7f2d9de",
+    "url": "/dco-mp/static/js/2.bcd84f59.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/dco-mp/static/js/2.bcd84f59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6256ecde17ff55c415a0",
+    "url": "/dco-mp/static/js/main.bf2d7434.chunk.js"
+  },
+  {
+    "revision": "98ba91e48e379689b19e",
+    "url": "/dco-mp/static/js/runtime-main.b3c348d6.js"
+  },
+  {
+    "revision": "b621b313acf1c48c8104bbb0822ec056",
+    "url": "/dco-mp/static/media/01.b621b313.png"
+  },
+  {
+    "revision": "d43ec5e43e3d162b485a22a9b2eee348",
+    "url": "/dco-mp/static/media/02.d43ec5e4.jpg"
+  },
+  {
+    "revision": "69a35de727ef5dc903b9f6c754d22274",
+    "url": "/dco-mp/static/media/03.69a35de7.jpg"
+  },
+  {
+    "revision": "7e36106cde06b50f1ee3f3ded66f2908",
+    "url": "/dco-mp/static/media/4.7e36106c.png"
+  },
+  {
+    "revision": "2644c345aa8d5d316f4bf77d24436d5b",
+    "url": "/dco-mp/static/media/CensusINDIA.2644c345.pdf"
+  },
+  {
+    "revision": "4fd190c09a79c6ceab7a03d094e300da",
+    "url": "/dco-mp/static/media/MANGAL.4fd190c0.rar"
+  },
+  {
+    "revision": "dd8ff2f15954fb8e9a383c4075e7f4ff",
+    "url": "/dco-mp/static/media/RTI-Board.dd8ff2f1.pdf"
+  },
+  {
+    "revision": "52cf9f5207b5639b45158b1d4d078f93",
+    "url": "/dco-mp/static/media/bg.52cf9f52.png"
+  },
+  {
+    "revision": "91dd77d70ee8124a589b2bc92d92763b",
+    "url": "/dco-mp/static/media/crs.91dd77d7.png"
+  },
+  {
+    "revision": "fcd23913a435a06be1291578c67e63a1",
+    "url": "/dco-mp/static/media/emblem.fcd23913.png"
+  },
+  {
+    "revision": "f7e7309c45003b1022f55031e41f6809",
+    "url": "/dco-mp/static/media/h.f7e7309c.jpg"
+  },
+  {
+    "revision": "b2e3978ad37e472debd65feabef0cd42",
+    "url": "/dco-mp/static/media/img.b2e3978a.jpg"
+  },
+  {
+    "revision": "8930cb07d269f6612bc5c17a2e67a9c9",
+    "url": "/dco-mp/static/media/logo.8930cb07.png"
+  }
+]);
